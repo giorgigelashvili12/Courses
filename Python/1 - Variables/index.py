82bfcbb0b617dd7abc7177ffb64671722a9bbe57
giorgigelashvili12 <tutunagio777@gmail.com>
2024-05-02 22:43:40 +0400

    first commit

---
+++ Python/1 - Variables/index.py
@@ -0,0 +1,6 @@
+# VARIABLES
+# variable is a container for a value, it behaves as the value that it contains
+# itself. It's mostly like Algebra, where you find the value of x (10 = 5 * x, x = 2).
+# it's nearly same in programming. Here we can assign different types of values to variables
+# or you could say - Data Types. We can assign: Words, whole numbers, even values called booleans,
+# which are either True or False.
