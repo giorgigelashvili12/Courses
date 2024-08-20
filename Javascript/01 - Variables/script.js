b755a1c390067c687724d06b9bb37fc46a5f6bf0
giorgigelashvili12 <tutunagio777@gmail.com>
2024-08-20 13:02:01 +0400

    JS

--- Javascript/01 - Variables/script.js
+++ Javascript/01 - Variables/script.js
@@ -1,33 +1,65 @@
 // In Javascript, variables are used to store data values. They are like containers
 // that hold information that can be referenced and manipulated in a program.
-// These are how variables work in javascript:
+
+// ჯავასკრიპტში ცვლადები გამოიყენება სხვადასხვა მონაცემის შესანახად. ისინი არიან კონტეინერები
+// რომლებიც იკავებენ ინფორმაციას რომელზეც მუშაობა იქნება შესაძლები.
+
+//////////////////////////////////////////////////////////////////////////////
 
 // 1. Variable Declaration
 // - To create a variable in javascript, you use keyword: 'var', 'let', or 'const'.
+
+// 1. ცვლადის დეკლარაცია
+// - რომ მოვახდინოთ ცვლადის დეკლარაცია, ჩვენ ვიყენებთ keywords - სკვანძო სიტყვებს, როგორიცაა: 'var', 'let', ან 'const'.
+
 var firstVar;
-// var: Historically used for variable declaration. Variables declared with 'var' have
-//      function scope or global scope depending on where they are declared (Global/Local Variables)
+// var: Historically used for variable declaration.
+// var: არის ძველი საკვანძო სიტყვა რომელიც გამოიყენება ცვლადის დეკლარაციაში.
+
+//////////////////////////////////////////////////////////////////////////////
+
 let secondVar;
-// let: Variables that are declared with 'lt' aee limited to the block in which
+// let: Variables that are declared with 'lt' are mostly recommended.
 //      they are defined.
+// let: ცვლადები რომლებიც არის შექმნილი 'let' საკვანძო სიტყვით, არის უფრო ახალი და მეტად რეკომენდირებული.
+
+//////////////////////////////////////////////////////////////////////////////
+
 const PI = 3.14;
 // const: const declares a constant variable whose value cannot be reassigned.
 //        It is block-scoped like 'let', but its value remains fixed once assigned.
+// const: const წარმოქმნის კონტანტას, რომლის მნიშვნელობის მოდიფიცირება შეუძლებელია.
+//        ის არის იგივე 'let' keyword-ი რომელიც არ იცვლება. ამ keyword-ით იქმნება
+//        ისეთი ცვლადები რომლის მოდიფიცირების სურვილი არ გვაქვს.
+
+//////////////////////////////////////////////////////////////////////////////
 
 // 2. Assigning Values
 // - After declaring a variable, you can assign a value to it using the assigment operator ('=').
+
+// 2. მნიშვნელობის მინიჭება
+// - როდესაც მოხდება ცვლადის დეკლარაცია, შესაძლებელია მნიშვნელობის მინიჭება ამ ოპერატორით '='
 firstVar = "Hello World";
 secondVar = 20;
+
 // You can also declare and assign value to a variable on one line
+// შეგიძლია ერთ ხაზზე მოახდინო დეკლარაცია და მნიშვნელობის მინიჭება
 let demo = "Hi!";
+
 demo = "Bye!"
 // This is also used for reassigning value
+// ასე შეგეძლებათ კონკრეტული ცვლადისთვის მნიშვნელობის შეცვლა
+
+//////////////////////////////////////////////////////////////////////////////
 
 // 3. Data Types
 // - Javascript is dynamically typed, which means that variables can hold values of any data type.
 //   Common data types include:
+
+// 3. სხვადასხვა ცვლადის მონაცემები
+
 // * Primitve Types * - 'number', 'string', 'boolean', 'null', 'undefined', 'symbol'
-// * Reference Types * - 'object', 'array', 'function'
+// * Reference Types * - 'object', 'array', 'function', 'map', 'date', 'set'
 
 // 4. Variable Naming Rules
 // - Variable names are case-sensetive, which means that 'age' and 'Age' are different here.
