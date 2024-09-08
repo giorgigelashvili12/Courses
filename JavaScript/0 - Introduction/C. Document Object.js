b31d6bd38cfa7863005735fd8807ca5e9aa9f03f
giorgigelashvili12 <tutunagio777@gmail.com>
2024-09-08 18:23:13 +0400

    JS

--- JavaScript/0 - Introduction/C. Document Object.js
+++ JavaScript/0 - Introduction/C. Document Object.js
@@ -1,13 +1,37 @@
 // document - Whole HTML page/document. An object. (DOM)
 // Early DOM and simple explanation
 // Document Object Model (DOM) - <html> nested tags
+// contains main methods
+
+// Highly used ones
+// document.querySelector()
+// gets first element that is specified with css selector (css code)
+document.querySelector();
+
+// document.querySelectorAll()
+// gets all elements that are specified with css selector (css code)
+document.querySelectorAll();
+
+// docuemnt.getElementById()
+// gets all elements from the document with specified id
+document.getElementById();
+
+// document.getElementsByClassName()
+// returns every element that has a specified class
+document.getElementsByClassName()
+
+// document.getElementsByTagName()
+// returns every element with a specified tag name
+document.getElementsByTagName();
+
+/////////////////////////////////////////////////////
 
 // document.title
 // <html> -> <head> -> <title> tag
 // changing current title to: 
 document.title = 'Modofied Title';
 
-// document.body
+// document.body - Highly Used
 // <html> -> <body>
 // represents HTML body tag
 // Outputting HTML body tag
@@ -17,3 +41,19 @@ console.log(document.body);
 // represents head element of the document
 // <html> -> <head>
 console.log(document.head);
+
+// document.URL
+// return the URL of the document
+console.log(document.URL);
+
+// document.cookie
+// represents cookies associated with the document
+// You can edit delete and read them
+console.log(document.cookie);
+
+// document.readyState
+// returns the loading status of the document ('loading'/'interactive'/'complete')
+console.log(document.readyState);
+
+
+// Nodes (not yet)
