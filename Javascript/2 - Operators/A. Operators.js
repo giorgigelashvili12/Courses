50131ce622748997bb9282efce3412f70597fbb7
giorgigelashvili12 <tutunagio777@gmail.com>
2024-09-21 20:58:58 +0400

    JS

--- Javascript/2 - Operators/A. Operators.js
+++ Javascript/2 - Operators/A. Operators.js
@@ -1,1 +1,10 @@
-// Operators in js are used to complete
+// Operators in js are used to complete different operations on variables and values
+// using symbols. They are catorized into several types:
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
---
+++ Javascript/2 - Operators/C. Comprasion.js
@@ -0,0 +1,22 @@
+// Comprasion values are used to compare different values and in the end to return
+// boolean type based on the comprasion results. Used when it comes to logcial
+// comprasion and determines equality or the difference between the values
+
+// Equal to (==)
+// compares two values and returns true/false:
+5 == 3; // false
+5 == 5; // true
+5 = '5'; // true
+// 5 == '5' because JS converts the value to a numerical type
+
+// Strict Equality (===)
+// compares value and data type of 2 given variables/values
+5 === 3; // false
+5 === 5; // true
+5 === '5'; // false
+
+// Inequality/Not equal to (!=)
+// compares two values and detects if they're not equal to eachother
+5 != 3; // true
+5 != 5; // 
+5 != '5';
