4f1ecbab4f98e406b2b0b9d923757e774a316fca
giorgigelashvili12 <tutunagio777@gmail.com>
2024-09-26 22:00:52 +0400

    3 - Operators

---
+++ JS/2 - Operators/F. Ternary.js
@@ -0,0 +1,9 @@
+// Ternary operators are a way to perform conditional expressions in 3 operations on a single line.
+condition ? expressionIfTrue : expressionIfFalse;
+// condition: if true then perform expressionIfTrue, else then expressionIfFalse
+
+const testScore = 86;
+score >= 80 ? 'A' : 'A-';
+// if score is more than 80 return 'A' else return 'A-'
+
+// In conclusion it is the shortest conditional expression
---
+++ JS/2 - Operators/G. Typeof.js
@@ -0,0 +1,12 @@
+// The typeof operand is used to detect the datatupe of a given variable or a value. After detection is returns a string and returns the datatype
+
+let y;
+typeof y; // Undefined (No value)
+
+typeof true; // Boolean
+
+typeof 50; // Number
+
+typeof 'Hi'; // String
+
+typeof null; // Object
