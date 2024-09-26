ba160c8eb422d17723780aad4c3b80e0670b6c73
giorgigelashvili12 <tutunagio777@gmail.com>
2024-09-26 22:28:03 +0400

    3 - Functions

---
+++ JS/3 - Functions/Function.js
@@ -0,0 +1,31 @@
+// Function is a block of code designed to perform a task. It is executed whenever it is called
+// Syntax
+// Functions in JS are defined by the function keyword, then the name and parentheses '()'.
+// Function name can contain letters, digits, underscored and dollar signs
+// Parentheses may include parameter names that are seperated by comas
+
+function func(x1, y2) {
+    return x1, y2;
+}
+// In this block of code, p1 and p2 are 'func' functions parameters
+
+// Function Invocation
+// The function and the code within the function will execute once it is invoked (called)
+// Can be called when a button is clicked, inside JS code and automatically
+
+// Function return
+// When function reaches the return statement the function will stop executing. Return statement returns a value,
+// returned back to the caller
+
+// Function () operator
+// () operator calls the function
+func(2, 8);
+
+// Variable Scopes
+// Local Variables
+// Variables declared within functions and other blocks of code are Local to the function, meaning it can't be accessed and used
+// outside the function
+
+// Global Variables
+// Global variables on the other hand are variables declared outside functions and other blocks of code which can be accessed both outside
+// and inside the functions
