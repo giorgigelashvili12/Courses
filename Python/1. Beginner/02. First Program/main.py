a3058b56863884e2c698dd9ed2be7235d1e7515e
giorgigelashvili12 <tutunagio777@gmail.com>
2024-09-27 20:01:36 +0400

    Beginner

---
+++ Python/1. Beginner/02. First Program/main.py
@@ -0,0 +1,8 @@
+# First Program
+# To get started with Python and understand its syntax we can write our first code in Python - Hello World application.
+
+print('Hello World')
+# print() is a so called function, when used it outputs a certain content to the screen, and the content we want to display
+# is put between the parentheses.
+# other much difficult programming languages need function, classes - bunch of lines of code to output a simple text just
+# like this.
